58c3b40e8b30f108e22060b13b94c62c40de0635
riotruij <rio.truija@ut.ee>
2024-12-03 19:28:36 +0200

    changed coordinate base direction and image output format

--- imageGen.py
+++ imageGen.py
@@ -12,8 +12,7 @@ def isInside(point, polygon):
     for i in range(1, vertices+1):
         # check if the x coordinate of the point is between the x coordinates of polygon[i], polygon[i+1]
         p2 = polygon[i % vertices]
-
-        # bs magic from https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
+        
         if y > min(p1[1], p2[1]):
             if y <= max(p1[1], p2[1]):
                 if x < max(p1[0], p2[0]):
@@ -29,14 +28,13 @@ def isInside(point, polygon):
 def generateImage(output, size, polygons): # polygons is a list of polygons where each polygon is a list of (x, y)-tuples
     im = PIL.Image.new(mode='RGB', size=(size, size))
     pixels = im.load()
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
+    for i in range(size):
+        for j in range(size):
             # for larger lists of polygons this could be improved by clustering the polygons first
             for polygon in polygons:
                 if isInside((i, j), polygon):
-                    pixels[i, j] = (255, 255, 255)
+                    pixels[i, size-j-1] = (255, 255, 255)
+    im.save(output, 'tiff')
     im.show()
-    im.save(output)
 
-if __name__ == "main":
-    generateImage("data/img/test.png", 500, [[(10, 10), (300, 250), (250, 300), (100, 100), (10, 490)], [(490, 10), (470, 100), (300, 50)]])
+generateImage("data/img/test.tif", 500, [[(10, 10), (300, 250), (250, 300), (50, 200), (10, 490)]])
