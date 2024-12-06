64d1add60e1d2154e8fab695ae451567a73f4af0
riotruij <rio.truija@ut.ee>
2024-12-06 20:47:35 +0200

    generates polygon image of tartu

--- imageGen.py
+++ imageGen.py
@@ -1,40 +1,17 @@
 import PIL.Image
+import PIL.ImageDraw
+from parseCSV import get_polygons_from_csv
 
-def isInside(point, polygon):
-    # for every two consecutive points inside the polygon
-    inside = False
-    p1 = polygon[0]
-    vertices = len(polygon)
-
-    x = point[0]
-    y = point[1]
-
-    for i in range(1, vertices+1):
-        # check if the x coordinate of the point is between the x coordinates of polygon[i], polygon[i+1]
-        p2 = polygon[i % vertices]
-        
-        if y > min(p1[1], p2[1]):
-            if y <= max(p1[1], p2[1]):
-                if x < max(p1[0], p2[0]):
-                    intersection_x = (y - p1[1]) * (p2[0] - p1[0]) / (p2[1] - p1[1]) + p1[0]
-                    if x <= intersection_x or p1[0] == p2[0]:
-                        inside = not inside
-
-        p1 = p2
-
-    return inside
-
-
-def generateImage(output, size, polygons): # polygons is a list of polygons where each polygon is a list of (x, y)-tuples
+def generate_image(output, size, polygons): # polygons is a list of polygons where each polygon is a list of (x, y)-tuples
+    width, height = size, size
     im = PIL.Image.new(mode='RGB', size=(size, size))
-    pixels = im.load()
-    for i in range(size):
-        for j in range(size):
-            # for larger lists of polygons this could be improved by clustering the polygons first
-            for polygon in polygons:
-                if isInside((i, j), polygon):
-                    pixels[i, size-j-1] = (255, 255, 255)
-    im.save(output, 'tiff')
-    im.show()
+    draw = PIL.ImageDraw.Draw(im)
+    for i, corners in enumerate(polygons):
+        draw.polygon(corners, fill="white")
+    im.save(output, format='tiff')
 
-generateImage("data/img/test.tif", 500, [[(10, 10), (300, 250), (250, 300), (50, 200), (10, 490)]])
+def image_from_csv():
+    polygons = get_polygons_from_csv("data/csv/data_eesti.csv")
+    generate_image("data/img/tartu.tif", 25000, polygons)
+
+image_from_csv()
--- parseCSV.py
+++ parseCSV.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import os
 from shapely import wkb
+from shapely.geometry import Polygon
+
+def translate_coord(coords, offset_x=6470000, offset_y=655000, scale=5): # scale is pixels per coord
+    # default offsets and scale is for square 54752 with scale 1:10000
+    x = int((coords[0]-offset_x)*scale)
+    y = int((coords[1]-offset_y)*scale)
+    return (y, 25000-x)
+
 
 def wkb_to_coords(w):
     if pd.isna(w):
@@ -8,15 +16,28 @@ def wkb_to_coords(w):
     geometry = wkb.loads(bytes.fromhex(str(w)))
     return geometry.wkt
 
-def parseCSV(sample_csv_path, parsed_csv_path):
+def wkb_to_polygons(w):
+    if pd.isna(w):
+        return None
+    geometry = wkb.loads(bytes.fromhex(str(w)))
+    if not isinstance(geometry, Polygon):
+        return None
+    polygon = [translate_coord((x, y)) for y, x in geometry.exterior.coords]
+    if (polygon[0][0] > 26000 or polygon[0][0] < -1000):
+        return None
+    if (polygon[0][1] > 26000 or polygon[0][1] < -1000):
+        return None
+    return polygon
+
+
+
+def get_polygons_from_csv(sample_csv_path):
     current_dir = os.path.dirname(__file__)
     f = open(os.path.join(current_dir, sample_csv_path), encoding='utf-8')
-    df = pd.read_csv(f, encoding='utf-8', delimiter=';')
-    parsed_geom = df.geometry.apply(wkb_to_coords)
-    parsed_df = pd.DataFrame({
-        'wkb': df.geometry,
-        'wkt': parsed_geom
-    })
-    parsed_df.to_csv(os.path.join(current_dir, parsed_csv_path))
+    df = pd.read_csv(f, encoding='utf-8', delimiter=';', low_memory=False)
+    parsed_geom = df.geometry.apply(wkb_to_polygons).dropna()
+    print("finished parsing polygons")
+    return parsed_geom.tolist()
 
-parseCSV('data/csv/data_sample.csv', 'data/csv/parsed_data.csv')
+
+
